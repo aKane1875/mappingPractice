6e684d7d35649c102b3f5198aa5dc08f6ba40e48
Allan Kane <allanhfc@yahoo.co.uk>
2022-01-20 16:57:45 +0000

    set up complete, map displaying

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={StyleSheet.absoluteFillObject}>
+      <Text>HIBERNIAN</Text>
+      <MapView
+        style={{ flex: 1 }}
+        provider={PROVIDER_GOOGLE}
+        region={{
+          latitude: 23.259933,
+          longitude: 77.412613,
+          latitudeDelta: 0.009,
+          longitudeDelta: 0.009,
+        }}
+        showUserLocation={true}
+      >
+        <Marker coordinate={{ latitude: 23.259933, longitude: 77.412613 }} />
+      </MapView>
     </View>
   );
 }
@@ -14,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
